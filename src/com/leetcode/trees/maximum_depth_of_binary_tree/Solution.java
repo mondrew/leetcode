36702cc2670243d5dc5e7696e53271be7bb6d57a
Andrei Beseda <andrebeseda@gmail.com>
2023-02-15 18:32:51 -0500

    Make some tasks

--- src/com/leetcode/trees/maximum_depth_of_binary_tree/Solution.java
+++ src/com/leetcode/trees/maximum_depth_of_binary_tree/Solution.java
@@ -5,10 +5,24 @@ import com.leetcode.trees.TreeNode;
 public class Solution {
 
     public static int maxDepth(TreeNode root) {
-        return 0;
+        return root == null ? 0 : Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
     }
 
     public static void main(String[] args) {
-
+        /*
+            Tree: [3,9,20,null,null,15,7]
+         */
+        TreeNode secondChildLeft = new TreeNode(15);
+        TreeNode secondChildRight = new TreeNode(7);
+        TreeNode firstChildLeft = new TreeNode(9);
+        TreeNode firstChildRight = new TreeNode(20, secondChildLeft, secondChildRight);
+        TreeNode root = new TreeNode(3, firstChildLeft, firstChildRight);
+        System.out.println(maxDepth(root));
+        /*
+            Tree: [1, null, 2]
+         */
+//        TreeNode firstChildRight = new TreeNode(2);
+//        TreeNode root = new TreeNode(1, null, firstChildRight);
+//        System.out.println(maxDepth(root));
     }
 }
