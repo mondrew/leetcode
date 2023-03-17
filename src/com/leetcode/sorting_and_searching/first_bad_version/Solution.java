e72de70fbbbe5acc6f98deab7ec7c0f60e944d7b
Andrei Beseda <andrebeseda@gmail.com>
2023-03-16 20:22:36 -0400

    Finished sorting and merging

--- src/com/leetcode/sorting_and_searching/first_bad_version/Solution.java
+++ src/com/leetcode/sorting_and_searching/first_bad_version/Solution.java
@@ -1,43 +1,27 @@
 package com.leetcode.sorting_and_searching.first_bad_version;
 
-import java.util.Arrays;
-
 public class Solution {
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m + n - 1;
-        n--;
-        m--;
+    public static int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
 
-        while (n >= 0 && m >= 0) {
-            nums1[i--] = nums2[n] > nums1[m] ? nums2[n--] : nums1[m--];
-        }
-
-        while (n >= 0) {
-            nums1[i--] = nums2[n--];
-        }
-    }
-
-    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
-        int i = m + n - 1;
-        n--;
-        m--;
-
-        while (i >= 0) {
-            if (n >= 0 && (m < 0 || nums2[n] >= nums1[m])) {
-                nums1[i--] = nums2[n--];
-            } else if (m >= 0) {
-                nums1[i--] = nums1[m--];
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            if (!isBadVersion(mid)) {
+                start = mid + 1;
+            } else {
+                end = mid;
             }
         }
+        return start;
+    }
+
+    public static boolean isBadVersion(int version) {
+        return version >= 57;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 2, 3, 0, 0, 0};
-        int m = 3;
-        int[] nums2 = {2, 5, 6};
-        int n = 3;
-        merge(nums1, m, nums2, n);
-        System.out.println(Arrays.toString(nums1));
+        System.out.println(firstBadVersion(9573));
     }
 }
--- src/com/leetcode/sorting_and_searching/merge_sorted_array/Solution.java
+++ src/com/leetcode/sorting_and_searching/merge_sorted_array/Solution.java
@@ -4,12 +4,40 @@ import java.util.Arrays;
 
 public class Solution {
 
-    public static int firstBadVersion(int n) {
-        // todo
-        return 0;
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        n--;
+        m--;
+
+        while (n >= 0 && m >= 0) {
+            nums1[i--] = nums2[n] > nums1[m] ? nums2[n--] : nums1[m--];
+        }
+
+        while (n >= 0) {
+            nums1[i--] = nums2[n--];
+        }
+    }
+
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int i = m + n - 1;
+        n--;
+        m--;
+
+        while (i >= 0) {
+            if (n >= 0 && (m < 0 || nums2[n] >= nums1[m])) {
+                nums1[i--] = nums2[n--];
+            } else if (m >= 0) {
+                nums1[i--] = nums1[m--];
+            }
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(firstBadVersion(5));
+        int[] nums1 = {1, 2, 3, 0, 0, 0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
     }
 }
