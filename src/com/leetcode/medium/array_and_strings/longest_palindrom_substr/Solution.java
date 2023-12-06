a23e1990d7c61c50babfe13c1fd7c5e9cf7bb789
andreibeseda <andrebeseda@gmail.com>
2023-12-06 15:53:19 -0500

    Medium array and strings in progress

---
+++ src/com/leetcode/medium/array_and_strings/longest_palindrom_substr/Solution.java
@@ -0,0 +1,12 @@
+package com.leetcode.medium.array_and_strings.longest_palindrom_substr;
+
+public class Solution {
+
+    public static String longestPalindrome(String s) {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("babad"));
+    }
+}
