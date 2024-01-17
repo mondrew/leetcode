74ba77d0233955343ef54f2d5780fc282b6d57e7
Andrei Beseda <andrebeseda@gmail.com>
2024-01-17 13:11:51 -0500

    Capital One - TwoSum finished

---
+++ src/com/leetcode/companies/capital_one/two_sum/Solution.java
@@ -0,0 +1,23 @@
+package com.leetcode.companies.capital_one.two_sum;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
